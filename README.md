b7719c01bdd5a4a2849db19d407c577a38695a2d
Javier Leon <javierleon01@yahoo.com>
2018-05-30 18:55:37 -0700

    switched listening port some reason old 8090 wasnt workin

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Hw submission // used express , handlebars, body-parser, and mysql
 
 will add gitnore in the future
+
+- googled and tried method-override but didnt work
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var methodOverride = require('method-override');
 var app = express();
 //app.use(express.static(__dirname + '/public'));
 
-var PORT = process.env.PORT || 8090;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
  
 // override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+// app.use(methodOverride('_method'))
 
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
